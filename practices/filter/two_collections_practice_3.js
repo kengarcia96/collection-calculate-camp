f729663d15d9da9d01c1653f41b0364c5fa5b78f
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-13 14:43:12 +0800

    Corrected anser for collection practice 3

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-  return collection_a.filter(n => collection_b.includes (n % 2 === 0));
+  return collection_a.filter((num) => collection_b.filter(n => num % n === 0).length > 0);
 }
 
 module.exports = choose_divisible_integer;
